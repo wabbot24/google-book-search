d0db77a2aa5f5bead57ce780f4ca55b4cff822c4
wabbot <wabbot24@gmail.com>
2019-06-29 13:12:13 -0400

    Updates

---
+++ precache-manifest.44bc5d0b2c3648256296bb17b2b8abd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "825b73dd7ea06fdfe81c",
+    "url": "/google-book-search/static/css/main.984f9396.chunk.css"
+  },
+  {
+    "revision": "825b73dd7ea06fdfe81c",
+    "url": "/google-book-search/static/js/main.a5c88098.chunk.js"
+  },
+  {
+    "revision": "d536b4b13827ad40f1bc",
+    "url": "/google-book-search/static/js/runtime~main.aaf871bd.js"
+  },
+  {
+    "revision": "08e60256e97de3e5b743",
+    "url": "/google-book-search/static/js/2.8006c6cb.chunk.js"
+  },
+  {
+    "revision": "39c9b52e97d1c85cb90e2dca0f04f314",
+    "url": "/google-book-search/index.html"
+  }
+];
